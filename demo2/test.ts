6e523290c643a1cd2f5d7b1bee5c7362a9d43414
zhengbigbig <780357902@qq.com>
2021-09-27 13:50:14 +0800

    get/post请求

--- demo2/test.ts
+++ demo2/test.ts
@@ -1,10 +1,25 @@
 import * as http from 'http';
+import {IncomingMessage, ServerResponse} from 'http';
+import {Buffer} from 'buffer';
 
 const server = http.createServer();
 
-server.on('request', (request,response) => {
-    console.log('请求来了',request);
-    response.end('hi');
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+    console.log('请求来了');
+    console.log(request.method, request.headers, request.url);
+    const arr = [];
+    request.on('data', (chunk: Buffer) => {
+        arr.push(chunk);
+    });
+    request.on('end', () => {
+        const body = Buffer.concat(arr).toString();
+        console.log(body);
+        response.statusCode = 400;
+        response.setHeader('X-M', 'lalala');
+        response.write('1\n');
+        response.end('hi');
+    });
+
 });
 
 server.listen(8888);
