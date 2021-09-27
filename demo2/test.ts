62c5048a64d93fcfdeb6bdc842e5a877b70683be
zhengbigbig <780357902@qq.com>
2021-09-27 15:37:24 +0800

    处理任意路径

--- demo2/test.ts
+++ demo2/test.ts
@@ -10,32 +10,25 @@ const publicDir = p.relative(__dirname, 'public');
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     console.log('请求来了');
     const {method, headers, url} = request;
-    const path = new URL(url,'http://ex.com/')
+    const path = new URL(url, 'http://ex.com/');
     if (method === 'GET') {
-        switch (path.pathname) {
-            case '/index.html':
-                response.setHeader('Content-Type', 'text/html;charset=utf-8');
-                fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
-                    if (error) throw error;
-                    response.end(data.toString());
-                });
-                break;
-            case '/style.css':
-                response.setHeader('Content-Type', 'text/css;charset=utf-8');
-                fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
-                    if (error) throw error;
-                    response.end(data.toString());
-                });
-                break;
-            case '/main.js':
-                response.setHeader('Content-Type', 'text/javascript;charset=utf-8');
-                fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
-                    if (error) throw error;
-                    response.end(data.toString());
-                });
-                break;
+        const filename = path.pathname.substr(1) || 'index.html';
+        fs.readFile(p.resolve(publicDir, filename), (error, data) => {
+            if (error) {
+                if (error.code === 'ENOENT') {
+                    response.statusCode = 404;
+                    fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
+                        response.end(data);
+                    });
+                } else {
+                    response.statusCode = 500;
+                    response.end('服务器繁忙');
+                }
+            } else {
+                response.end(data);
+            }
 
-        }
+        });
     }
     // const arr = [];
     // request.on('data', (chunk: Buffer) => {
