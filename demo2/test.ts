bbc9ce99990f0c49e08e46dfa6387c96bffc7581
zhengbigbig <780357902@qq.com>
2021-09-27 14:45:36 +0800

    处理查询字符串

--- demo2/test.ts
+++ demo2/test.ts
@@ -2,37 +2,39 @@ import * as http from 'http';
 import {IncomingMessage, ServerResponse} from 'http';
 import {Buffer} from 'buffer';
 import * as fs from 'fs';
-import * as path from 'path';
+import * as p from 'path';
 
 const server = http.createServer();
-const publicDir = path.relative(__dirname, 'public');
+const publicDir = p.relative(__dirname, 'public');
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     console.log('请求来了');
     const {method, headers, url} = request;
+    const path = new URL(url,'http://ex.com/')
     if (method === 'GET') {
-        switch (url) {
+        switch (path.pathname) {
             case '/index.html':
                 response.setHeader('Content-Type', 'text/html;charset=utf-8');
-                fs.readFile(path.resolve(publicDir, 'index.html'), (error, data) => {
+                fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
                     if (error) throw error;
                     response.end(data.toString());
                 });
                 break;
             case '/style.css':
                 response.setHeader('Content-Type', 'text/css;charset=utf-8');
-                fs.readFile(path.resolve(publicDir, 'style.css'), (error, data) => {
+                fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
                     if (error) throw error;
                     response.end(data.toString());
                 });
                 break;
             case '/main.js':
                 response.setHeader('Content-Type', 'text/javascript;charset=utf-8');
-                fs.readFile(path.resolve(publicDir, 'main.js'), (error, data) => {
+                fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
                     if (error) throw error;
                     response.end(data.toString());
                 });
                 break;
+
         }
     }
     // const arr = [];
