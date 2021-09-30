bc742eae92585ed3fe9dd485a963c2a9aa492670
zhengbigbig <780357902@qq.com>
2021-09-30 10:15:21 +0800

    支持png，支持analyze

--- next-demo/next.config.js
+++ next-demo/next.config.js
@@ -1,3 +1,27 @@
-module.exports = {
-  reactStrictMode: true,
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+});
+const withPlugins = require('next-compose-plugins');
+
+// next.js configuration
+const nextConfig = {
+    reactStrictMode: true,
 }
+module.exports = withPlugins([withBundleAnalyzer], {
+    webpack: (config, {webpack, isServer, dev}) => {
+        config.module.rules.push({
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[contenthash].[ext]',
+                        outputPath: 'static', // disk path
+                        publicPath: '_next/static' // net path
+                    }
+                },
+            ],
+        })
+        return config
+    }
+}, nextConfig)
